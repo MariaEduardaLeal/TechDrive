ebd42410277e78bc733913aecc0bf56fe0254921
Fernando Nonato <105546921+Cyberfn@users.noreply.github.com>
2023-09-14 23:22:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="css/style_index.css">
+    <link rel="stylesheet" href="css/index.css">
 </head>
-
 <body>
     <section class="area-login">
         <div class="login">
-                <h1>Faça login para acessar</h1>
-                <form action="autenticar.php" method="$_POST">
-                    <input type="text" name="login" placeholder="Login"><br>
-                    <input type="password" name="senha" placeholder="Senha"><br>
-                    <input type="submit" name="Acessar" value="Acessar">
-                </form> 
-            </div>
+            <h1>Faça login para acessar</h1>
+            <form id="index.php" action="autenticar.php" method="post">
+                <input type="text" name="login" placeholder="Login"><br>
+                <input type="password" name="senha" placeholder="Senha"><br>
+                <input type="submit" name="Acessar" value="Acessar">
+            </form>
+        </div>
     </section>
 </body>
-
 </html>
